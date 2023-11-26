4b3634be32064fffbfa96978644ed93648ccc651
Parsa Malekian <parsaivim@gmail.com>
2023-11-26 11:31:25 +0330

    taqirate develop

--- calculator.c
+++ calculator.c
@@ -1,10 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
 int calculate(int a, int b) {
-<<<<<<< HEAD
- // Some calculation
- return a * b * 2 - 10;
-=======
- // A different calculation
- return a + b;
->>>>>>> feature-conflict
+ // A combination of both calculations
+ return a * b * 2 -10 + (a + b);
 }
